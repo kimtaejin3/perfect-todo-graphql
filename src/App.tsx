c0d786a449d13e04183767a96973eb1eefa08f37
Taejin Kim <rlaxowls1316@naver.com>
2024-04-10 00:27:21 +0900

    style: eslint 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const x = 10;
+  const a = 20;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +21,11 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          {[1, 2, 3, 4, 5].map((num) => {
+            return <li>jwoe</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
